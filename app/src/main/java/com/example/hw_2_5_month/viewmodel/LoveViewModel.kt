2e1076682d90f7654114e5739128a2ce7ca07eee
DanielPHP01 <danielmokushov01@gmail.com>
2023-03-08 14:58:27 +0600

    Update hilt

--- app/src/main/java/com/example/hw_2_5_month/viewmodel/LoveViewModel.kt
+++ app/src/main/java/com/example/hw_2_5_month/viewmodel/LoveViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.hw_2_5_month.remote.LoveModel
 import com.example.hw_2_5_month.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+@HiltViewModel
+class LoveViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
 
-class LoveViewModel@Inject constructor(val repository: Repository): ViewModel() {
 
     fun getLiveLove(firstName: String, secondName: String): LiveData<LoveModel> {
         return repository.getLove(firstName, secondName)
--- app/src/main/java/com/example/hw_2_5_month/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/hw_2_5_month/viewmodel/MainActivity.kt
@@ -6,9 +6,7 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.hw_2_5_month.R
 import com.example.hw_2_5_month.databinding.ActivityMainBinding
@@ -20,29 +18,27 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: LoveViewModel by viewModels()
+ var navController: NavController? = null
     @Inject
     lateinit var preferences: SharedPrefsPreferences
-    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initClicker()
-        initNavController()
         prefCheck()
+        //prefCheck()
     }
 
-    private fun initNavController() {
-        // Use Navigation.findNavController() to retrieve the NavController instance
-        navController = Navigation.findNavController(this, R.id.boardFragment)
-    }
 
     private fun prefCheck() {
         if (!preferences.isBoardingShowed()) {
-            navController.navigate(R.id.boardFragment)
+           navController?.navigate(R.id.boardFragment)
         }
     }
+
 
     private fun initClicker() {
         with(binding) {
